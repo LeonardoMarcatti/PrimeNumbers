0d4e993db8e5cc07cf528cd1b43e581b32d000be
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2020-05-19 17:38:27 -0300

    prime.css and prime.js has been created and prime_number.html was midified

---
+++ prime.js
@@ -0,0 +1,17 @@
+$('#calc').on('click', () => {
+    let a = new Array();
+    for(var i=1; i<=$('#number').val();i++){
+        a[i-1]=i;
+    };
+    for(let i1=1; i1<=$('#number').val(); i1++){
+        for(let i2 = 1; i2<=i; i2++){
+            if(i1%i2 == 0 && (i2 != i1) && (i2 != 1)){
+                let prime = a.indexOf(i1);
+                if(prime > -1){
+                   a.splice(prime, 1);
+                };
+            };
+        };
+    };
+    $('#answer').val(a);
+});
